ec83a4903f13ac82f83f2b7867df27bb6ff1ee01
Mohammed <marindodkh@gmail.com>
2026-03-28 15:57:38 +0100

    add surat al_anam

--- quran-tafsir-api/importData.ts
+++ quran-tafsir-api/importData.ts
@@ -26,7 +26,7 @@ async function importData() {
         `);
 
         console.log("Lecture du fichier tafsir.json...");
-        const rawData = fs.readFileSync('./JSON/tafsir_almaidah.json', 'utf-8');
+        const rawData = fs.readFileSync('./JSON/tafsir_alanam.json', 'utf-8');
         const tafsirData: TafsirItem[] = JSON.parse(rawData);
 
         console.log(`Insertion de ${tafsirData.length} mots dans la base de données...`);
